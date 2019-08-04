bf4014a3be9da0233bec95854453a13cf00fae15
Hai Cohen <hai321@gmail.com>
2019-08-04 11:59:51 +0300

    Fixed to relevant alarm

--- app/src/main/java/com/example/studicated/Alarm.java
+++ app/src/main/java/com/example/studicated/Alarm.java
@@ -1,6 +1,7 @@
 package com.example.studicated;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Ringtone;
@@ -11,9 +12,10 @@ import android.os.Vibrator;
 public class Alarm extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        Ringtone ringtone=RingtoneManager.getRingtone(context,alert);
-        ringtone.play();
+        Uri alarmSound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() + "/raw/alarm");
+        Ringtone r = RingtoneManager.getRingtone(context, alarmSound);
+        r.play();
+
         Vibrator v = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
         v.vibrate(2500);
     }
